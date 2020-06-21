22e3c32ab6eb58e29f0c76bdc469412b4f113910
abel <acervantes@shotl.com>
2020-06-21 21:55:36 +0200

    update examples

--- _examples/custom/custom.go
+++ _examples/custom/custom.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"log"
+	"time"
+
+	"github.com/abelcervantes/backoff"
+)
+
 func main() {
-	
+	b, err := backoff.New(2*time.Second, 20*time.Second, 400*time.Millisecond, false, 10)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i := 0; i < 10; i++ {
+		d := b.NextDuration()
+		log.Printf("next duration: %d", d/time.Second)
+		log.Printf("sleeping...")
+		time.Sleep(d)
+	}
 }
--- _examples/default/default.go
+++ _examples/default/default.go
@@ -7,17 +7,30 @@ import (
 	"github.com/abelcervantes/backoff"
 )
 
-func main() {
+func example1() {
 	b := backoff.NewDefault()
-	for true {
-		/*if b.HasReachedMaxAttempts() {
-			log.Fatalf("reached max number of attempts")
-		}*/
+	for i := 0; i < 15; i++ {
+		if b.HasReachedMaxAttempts() {
+			log.Printf("reached max number of attempts")
+		}
 
 		duration := b.NextDuration()
-		log.Printf("next duration: %.3f\tperformed: %d",
+		log.Printf("next duration: %.3f\tattempts performed: %d",
 			float64(duration)/float64(time.Second), b.PerformedAttempts())
 		log.Printf("sleeping...")
 		time.Sleep(duration)
 	}
+}
+
+func example2() {
+	b := backoff.NewDefault()
+	for i := 0; i < 15; i++ {
+		time.Sleep(b.NextDuration())
+	}
+}
+
+
+func main() {
+	example1()
+	//example2()
 }

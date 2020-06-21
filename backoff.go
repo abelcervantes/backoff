9e5c963ddea24b437b0152dbd6702c8e097a4250
abel <acervantes@shotl.com>
2020-06-21 18:14:46 +0200

    refactor

--- backoff.go
+++ backoff.go
@@ -8,43 +8,62 @@ import (
 )
 
 type Backoff struct {
-	min      int
-	max      int
-	unit     time.Duration
-	jitter   bool
-	attempts uint
+	min               *int
+	max               *int
+	unit              time.Duration
+	jitter            bool
+	slotTime          float64
+	attempts          uint
+	maxAttempts       uint
+	performedAttempts uint
 }
 
 var ErrMaxDurationMustBeGreater = errors.New("max duration must be greater than min duration")
+var ErrInvalidSlotTime = errors.New("slot time cannot be negative")
+var ErrInvalidMaxAttempts = errors.New("max attempt cannot be 0")
 
 // New creates a fully parametrized Backoff
-func New(min, max uint, unit time.Duration, jitter bool) (*Backoff, error) {
+func New(min, max uint, unit time.Duration, jitter bool, slotTime float64, maxAttempts uint) (*Backoff, error) {
 	if max < min {
 		return nil, ErrMaxDurationMustBeGreater
 	}
 
+	if slotTime < 0 {
+		return nil, ErrInvalidSlotTime
+	}
+
+	if maxAttempts == 0 {
+		return nil, ErrInvalidMaxAttempts
+	}
+
+	nMin, nMax := int(min), int(max)
 	return &Backoff{
-		min:      int(min),
-		max:      int(max),
-		unit:     unit,
-		jitter:   jitter,
+		min:         &nMin,
+		max:         &nMax,
+		unit:        unit,
+		jitter:      jitter,
+		slotTime:    slotTime,
+		maxAttempts: maxAttempts,
 	}, nil
 }
 
 const (
-	defaultMin int = 10
-	defaultMax int = 120
-	defaultUnit    = time.Second
-	defaultJitter  = true
+	defaultMin         int     = 0
+	defaultUnit                = time.Millisecond
+	defaultJitter              = true
+	defaultSlotTime    float64 = 100
+	defaultMaxAttempts uint    = 10
 )
 
 // NewDefault creates a Backoff with default configuration
 func NewDefault() Backoff {
+	dMin := defaultMin
 	return Backoff{
-		min:      defaultMin,
-		max:      defaultMax,
-		unit:     defaultUnit,
-		jitter:   defaultJitter,
+		min:         &dMin,
+		unit:        defaultUnit,
+		jitter:      defaultJitter,
+		slotTime:    defaultSlotTime,
+		maxAttempts: defaultMaxAttempts,
 	}
 }
 
@@ -58,11 +77,19 @@ func (b *Backoff) NextDuration() time.Duration {
 }
 
 func (b *Backoff) expJitter() time.Duration {
-	nextD := b.calcNextDuration()
+	nd := b.calcNextDuration()
+	if nd == 0 {
+		return time.Duration(nd)
+	}
 
 	rand.Seed(time.Now().UnixNano())
 
-	return time.Duration(rand.Intn(nextD-b.min)+b.min) * b.unit
+	x := nd - *b.min
+	if x > 0 {
+		x = rand.Intn(x)
+	}
+
+	return (time.Duration(x + *b.min)) * b.unit
 }
 
 func (b *Backoff) exp() time.Duration {
@@ -70,22 +97,35 @@ func (b *Backoff) exp() time.Duration {
 }
 
 func (b *Backoff) calcNextDuration() int {
-	d := int(math.Pow(2, float64(b.attempts)) * 100)
+	b.incAttempts()
 
-	if d < b.min {
-		return b.min
+	d := int((math.Pow(2, float64(b.attempts)) - 1) * b.slotTime)
+
+	if b.min != nil && d < *b.min {
+		return *b.min
 	}
 
-	if d > b.max {
-		return b.max
+	if b.max != nil && d > *b.max {
+		return *b.max
 	}
-
-	b.attempts++
 
 	return d
 }
 
-// Attempts returns the current number of performed attempts
-func (b *Backoff) Attempts() uint {
-	return b.attempts
+func (b *Backoff) incAttempts() {
+	if b.attempts < b.maxAttempts {
+		b.attempts++
+	}
+
+	b.performedAttempts++
+}
+
+// HasReachedMaxAttempts returns whether the backoff has reached the max number of attempts
+func (b *Backoff) HasReachedMaxAttempts() bool {
+	return b.attempts == b.maxAttempts
+}
+
+// PerformedAttempts returns the current number of performed attempts
+func (b *Backoff) PerformedAttempts() uint {
+	return b.performedAttempts
 }
--- backoff_test.go
+++ backoff_test.go
@@ -7,32 +7,287 @@ import (
 	"github.com/abelcervantes/backoff"
 )
 
-func TestNew(t *testing.T) {
-	t.Parallel()
+type newInput struct {
+	min         uint
+	max         uint
+	unit        time.Duration
+	jitter      bool
+	slotTime    float64
+	maxAttempts uint
+}
 
-	t.Run("returns max duration error", func(t *testing.T) {
-		b, err := backoff.New(5, 2, time.Second, true)
-		if err != backoff.ErrMaxDurationMustBeGreater {
-			t.Fatal("should return 'ErrMaxDurationMustBeGreater' error")
+type newTestTableItem struct {
+	inputs newInput
+	err error
+}
+
+func NewTestTable() []newTestTableItem {
+	return []newTestTableItem{
+		{
+			inputs: newInput{
+				min:         5,
+				max:         2,
+				unit:        time.Second,
+				jitter:      false,
+				slotTime:    100,
+				maxAttempts: 10,
+			},
+			err:    backoff.ErrMaxDurationMustBeGreater,
+		},
+		{
+			inputs: newInput{
+				min:         0,
+				max:         10,
+				unit:        time.Second,
+				jitter:      false,
+				slotTime:    -10,
+				maxAttempts: 10,
+			},
+			err:    backoff.ErrInvalidSlotTime,
+		},
+		{
+			inputs: newInput{
+				min:         0,
+				max:         10,
+				unit:        time.Second,
+				jitter:      false,
+				slotTime:    100,
+				maxAttempts: 0,
+			},
+			err:    backoff.ErrInvalidMaxAttempts,
+		},
+		{
+			inputs: newInput{
+				min:         0,
+				max:         10,
+				unit:        time.Second,
+				jitter:      false,
+				slotTime:    100,
+				maxAttempts: 10,
+			},
+			err:    nil,
+		},
+	}
+}
+
+func TestNew(t *testing.T) {
+	for _, expected := range NewTestTable() {
+		inp := expected.inputs
+		b, err := backoff.New(inp.min, inp.max, inp.unit, inp.jitter, inp.slotTime, inp.maxAttempts)
+		if err != expected.err {
+			t.Fatalf("expected: %s got: %s", expected.err, err)
 		}
 
-		if b != nil {
-			t.Fatal("should return a nil Backoff")
+		if err == nil {
+			if b == nil {
+				t.Fatalf("expected a non nil backoff")
+			}
+
+			if b.PerformedAttempts() != 0 {
+				t.Fatalf("expected 0 attempts, got: %d", b.PerformedAttempts())
+			}
+
+			if b.HasReachedMaxAttempts() {
+				t.Fatalf("expected to not reach max attempts without calling next duration at least once, got: %v", b.HasReachedMaxAttempts())
+			}
+		}
+	}
+}
+
+func TestNewDefault(t *testing.T) {
+	b := backoff.NewDefault()
+
+	if b.PerformedAttempts() != 0 {
+		t.Fatalf("expected 0 attempts, got: %d", b.PerformedAttempts())
+	}
+
+	if b.HasReachedMaxAttempts() {
+		t.Fatalf("expected to not reach max attempts without calling next duration at least once, got: %v", b.HasReachedMaxAttempts())
+	}
+}
+
+type nextDurationDefaultTestItem struct {
+	min     time.Duration
+	max     time.Duration
+	attempt uint
+}
+
+func nextDurationDefaultTable() []nextDurationDefaultTestItem {
+	return []nextDurationDefaultTestItem{
+		{
+			min:     0,
+			max:     100 * time.Millisecond,
+			attempt: 1,
+		},
+		{
+			min:     0,
+			max:     300 * time.Millisecond,
+			attempt: 2,
+		},
+		{
+			min:     0,
+			max:     700 * time.Millisecond,
+			attempt: 3,
+		},
+		{
+			min:     0,
+			max:     1500 * time.Millisecond,
+			attempt: 4,
+		},
+		{
+			min:     0,
+			max:     3100 * time.Millisecond,
+			attempt: 5,
+		},
+		{
+			min:     0,
+			max:     6300 * time.Millisecond,
+			attempt: 6,
+		},
+		{
+			min:     0,
+			max:     12700 * time.Millisecond,
+			attempt: 7,
+		},
+		{
+			min:     0,
+			max:     25500 * time.Millisecond,
+			attempt: 8,
+		},
+		{
+			min:     0,
+			max:     51100 * time.Millisecond,
+			attempt: 9,
+		},
+		{
+			min:     0,
+			max:     102300 * time.Millisecond,
+			attempt: 10,
+		},
+		{
+			min:     0,
+			max:     102300 * time.Millisecond,
+			attempt: 11,
+		},
+	}
+}
+
+type nextDurationParametrizedTestItem struct {
+	newInput            newInput
+	nextDurationOutputs []nextDurationDefaultTestItem
+}
+
+func nextDurationParametrizedTable() []nextDurationParametrizedTestItem {
+	return []nextDurationParametrizedTestItem{
+		{
+			newInput: newInput{
+				min:         0,
+				max:         0,
+				unit:        time.Millisecond,
+				jitter:      false,
+				slotTime:    100,
+				maxAttempts: 2,
+			},
+			nextDurationOutputs: []nextDurationDefaultTestItem{
+				{
+					min:     0,
+					max:     0,
+					attempt: 1,
+				},
+				{
+					min:     0,
+					max:     0,
+					attempt: 2,
+				},
+				{
+					min:     0,
+					max:     0,
+					attempt: 3,
+				},
+			},
+		},
+		{
+			newInput: newInput{
+				min:         0,
+				max:         20,
+				unit:        time.Second,
+				jitter:      true,
+				slotTime:    100,
+				maxAttempts: 2,
+			},
+			nextDurationOutputs: []nextDurationDefaultTestItem{
+				{
+					min:     0,
+					max:     0,
+					attempt: 1,
+				},
+				{
+					min:     0,
+					max:     0,
+					attempt: 2,
+				},
+				{
+					min:     0,
+					max:     0,
+					attempt: 3,
+				},
+			},
+		},
+	}
+}
+
+func TestBackoff_NextDuration(t *testing.T) {
+	t.Parallel()
+
+	t.Run("default next duration", func(t *testing.T) {
+		b := backoff.NewDefault()
+
+		if b.PerformedAttempts() != 0 {
+			t.Fatalf("expected 0 attempts, got: %d", b.PerformedAttempts())
+		}
+
+		if b.HasReachedMaxAttempts() {
+			t.Fatalf("expected to not reach max attempts without calling next duration at least once, got: %v", b.HasReachedMaxAttempts())
+		}
+		
+		for _, i := range nextDurationDefaultTable() {
+			d := b.NextDuration()
+			if b.PerformedAttempts() !=  i.attempt {
+				t.Fatalf("expected attempt: %d , got: %d", i.attempt, b.PerformedAttempts())
+			}
+
+			if d > i.max || d < i.min {
+				t.Fatalf("expected duration between: [%d, %d] got: %d", i.min, i.max, d)
+			}
 		}
 	})
 
-	t.Run("initial number of attempts must be 0", func(t *testing.T) {
-		b, err := backoff.New(5, 10, time.Second, true)
-		if err != nil {
-			t.Fatal("unexpected error")
-		}
+	t.Run("parametrized next duration (no jitter)", func(t *testing.T) {
+		for _, i := range nextDurationParametrizedTable() {
+			input := i.newInput
+			b, err := backoff.New(input.min, input.max, input.unit, input.jitter, input.slotTime, input.maxAttempts)
+			if err != nil {
+				t.Fatalf("expected no error, got: %s", err)
+			}
 
-		if b == nil {
-			t.Fatal("should return a non nil Backoff")
-		}
+			if b == nil {
+				t.Fatalf("expected a non nil backoff")
+			}
 
-		if b.Attempts() != 0 {
-			t.Fatalf("expected 0 attempts, got: %d", b.Attempts())
+			if b.PerformedAttempts() != 0 {
+				t.Fatalf("expected 0 attempts, got: %d", b.PerformedAttempts())
+			}
+
+			if b.HasReachedMaxAttempts() {
+				t.Fatalf("expected to not reach max attempts without calling next duration at least once, got: %v", b.HasReachedMaxAttempts())
+			}
+
+			for _, o := range i.nextDurationOutputs {
+				_ = b.NextDuration()
+				if o.attempt != b.PerformedAttempts() {
+					t.Fatalf("expected attempt: %d , got: %d", o.attempt, b.PerformedAttempts())
+				}
+			}
 		}
 	})
 }

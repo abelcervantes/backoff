e7ac07d78fc08d8541586e97fa049d3595ce1547
abel <acervantes@shotl.com>
2020-07-05 20:56:37 +0200

    update examples

--- _examples/custom/custom.go
+++ _examples/custom/custom.go
@@ -1,22 +1,64 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"time"
 
 	"github.com/abelcervantes/backoff"
 )
 
-func main() {
+func example() {
+	// http server
+	go func() {
+		maxTries, tries := 5, 0
+
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			if tries == maxTries {
+				fmt.Fprint(w, "Hello from server!")
+				return
+			}
+
+			tries++
+			http.Error(w, "internal server error", http.StatusInternalServerError)
+		})
+
+		_ = http.ListenAndServe(":8080", nil)
+	}()
+
+	time.Sleep(1 * time.Second)
+
+	// http client
 	b, err := backoff.New(2*time.Second, 20*time.Second, 400*time.Millisecond, false, 10)
 	if err != nil {
 		log.Fatal(err)
 	}
+	for !b.HasReachedMaxAttempts() {
+		response, err := http.Get("http://localhost:8080/")
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	for i := 0; i < 10; i++ {
-		d := b.NextDuration()
-		log.Printf("next duration: %d", d/time.Second)
+		if response.StatusCode == http.StatusOK {
+			log.Printf("external service answered with an OK response, performed attempts: %d", b.PerformedAttempts())
+
+			body, _ := ioutil.ReadAll(response.Body)
+			log.Printf("response: %s", string(body))
+			response.Body.Close()
+			break
+		}
+
+		log.Printf("external service answered with an error: %d", response.StatusCode)
+
+		response.Body.Close()
+
 		log.Printf("sleeping...")
-		time.Sleep(d)
+		time.Sleep(b.NextDuration())
 	}
+}
+
+func main() {
+	example()
 }
--- _examples/default/default.go
+++ _examples/default/default.go
@@ -1,36 +1,61 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"time"
 
 	"github.com/abelcervantes/backoff"
 )
 
-func example1() {
+func example() {
+	// http server
+	go func() {
+		maxTries, tries := 5, 0
+
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			if tries == maxTries {
+				fmt.Fprint(w, "Hello from server!")
+				return
+			}
+
+			tries++
+			http.Error(w, "internal server error", http.StatusInternalServerError)
+		})
+
+		_ = http.ListenAndServe(":8080", nil)
+	}()
+
+	time.Sleep(1 * time.Second)
+
+	// http client
 	b := backoff.NewDefault()
-	for i := 0; i < 15; i++ {
-		if b.HasReachedMaxAttempts() {
-			log.Printf("reached max number of attempts")
+	for !b.HasReachedMaxAttempts() {
+		response, err := http.Get("http://localhost:8080/")
+		if err != nil {
+			log.Fatal(err)
 		}
 
-		duration := b.NextDuration()
-		log.Printf("next duration: %.3f\tattempts performed: %d",
-			float64(duration)/float64(time.Second), b.PerformedAttempts())
-		log.Printf("sleeping...")
-		time.Sleep(duration)
-	}
-}
+		if response.StatusCode == http.StatusOK {
+			log.Printf("external service answered with an OK response, performed attempts: %d", b.PerformedAttempts())
 
-func example2() {
-	b := backoff.NewDefault()
-	for i := 0; i < 15; i++ {
+			body, _ := ioutil.ReadAll(response.Body)
+			log.Printf("response: %s", string(body))
+			response.Body.Close()
+			break
+		}
+
+		log.Printf("external service answered with an error: %d", response.StatusCode)
+
+		response.Body.Close()
+
+		log.Printf("sleeping...")
 		time.Sleep(b.NextDuration())
 	}
 }
 
-
 func main() {
-	example1()
-	//example2()
+	example()
 }
